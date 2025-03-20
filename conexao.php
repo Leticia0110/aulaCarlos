aceeb5fb28c6f41c8c84de2029ca4c64dabedcfc
Letícia Ramos de Oliveira <163892605+Leticia0110@users.noreply.github.com>
2025-03-20 11:25:02 -0300

    Add files via upload

---
+++ conexao.php
@@ -0,0 +1,10 @@
+<?php
+    $host="localhost";
+    $usuario="root";
+    $senha="";
+    $banco="sistema";
+
+    $conexao = mysqli_connect($host,$usuario,$senha,$banco) or die("erro: " .mysqli_connect_error());
+
+    if ()
+?>
